27b09c3d01d2aaabc0edee2cafe2b85d6a8242cf
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2026-01-15 23:41:48 +0530

    app.component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { CompanyComponent } from './pages/company/company.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+
+  { path: 'about', component: AboutComponent },
+
+  { path: 'products', component: ProductsComponent },
+
+  { path: 'company', component: CompanyComponent },
+
+  { path: 'contact', component: ContactComponent },
+
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
